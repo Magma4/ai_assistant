e7dccc11035a2ef97c8314d30077811e27d533b6
Magma4 <youngmagma@protonmail.com>
2023-06-23 20:57:25 +0000

    Added speak, time and date functions

--- magma.py
+++ magma.py
@@ -1,5 +1,29 @@
 import pyttsx3 #import pyttsx3 model
+import datetime as dt
 
-engine = pyttsx3.init()
-engine.say("Hello world!")
-engine.runAndWait()
+engine = pyttsx3.init() #Created and initialised an object for the ai package
+
+voices = engine.getProperty('voices') #This code changes the voice
+engine.setProperty('voice', voices[1].id)
+newVoiceRate = 160
+engine.setProperty('rate', newVoiceRate)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# speak("Hello, I am your AI assistant. You can ask me anything.")
+
+def time():
+    Time = dt.datetime.now().strftime("%I:%M:%S")
+    speak("The time is " + Time)
+
+# time()
+
+def date():
+    year = int(dt.datetime.now().year)
+    month = str(dt.datetime.now().month)
+    day = int(dt.datetime.now().day)
+    speak(f"The current date is {day} {month} {year}")
+
+date()
